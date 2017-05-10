dae50f34b975183a347c2f885ca50698639a1db6
Julien Perret <julien.perret@gmail.com>
2017-05-10 11:18:29 +0200

    producing a map of possible destinations

---
+++ fat/src/main/scala/eighties/h24/tools/EGTDestination.scala
@@ -0,0 +1,71 @@
+package eighties.h24.tools
+
+import better.files.File
+import com.vividsolutions.jts.geom.{Coordinate, GeometryFactory}
+import eighties.h24.dynamic.MoveMatrix
+import eighties.h24.generation.Flow
+import eighties.h24.{generation, space}
+import org.geotools.data.shapefile.ShapefileDataStoreFactory
+import org.geotools.data.{DataUtilities, Transaction}
+import org.geotools.geometry.jts.JTS
+import org.geotools.referencing.CRS
+
+/**
+  */
+object EGTDestination extends App {
+  def flowsFromEGT(aFile: File, res: File) = {
+    val l2eCRS = CRS.decode("EPSG:27572")
+    val outCRS = CRS.decode("EPSG:3035")
+    val transform = CRS.findMathTransform(l2eCRS, outCRS, true)
+    val x_laea_min = 3697000
+    val x_laea_max = 3846000
+    val y_laea_min = 2805000
+    val y_laea_max = 2937000
+    //val row = (x_laea_max - x_laea_min) / 1000
+    def location(coord: Coordinate): space.Location = {
+      val laea_coord = JTS.transform(coord, null, transform)
+      // replace by cell...
+      val dx = (laea_coord.x - x_laea_min)
+      val dy = (laea_coord.y - y_laea_min)
+      //      dx+dy*row
+      space.cell(dx, dy)
+    }
+    //val formatter = new SimpleDateFormat("dd/MM/yy hh:mm")
+    //val startDate = new DateTime(formatter.parse("01/01/2010 04:00"))
+
+    val factory = new ShapefileDataStoreFactory
+    val geomfactory = new GeometryFactory
+
+    //val resx=64
+    //val resy=81
+    val file = DataUtilities.urlToFile(res.toJava.toURI.toURL)
+    val dataStoreRes = factory.createDataStore(res.toJava.toURI.toURL)
+    val featureTypeNameRes = "Res"
+    val specsRes = "geom:Point:srid=3035"
+    val featureTypeRes = DataUtilities.createType(featureTypeNameRes, specsRes)
+    dataStoreRes.createSchema(featureTypeRes)
+    val typeNameRes = dataStoreRes.getTypeNames()(0)
+    val writerRes = dataStoreRes.getFeatureWriterAppend(typeNameRes, Transaction.AUTO_COMMIT)
+
+    val path = File("../données/EGT 2010/presence semaine EGT")
+    val newMatrix = generation.flowsFromEGT(path / "presence_semaine_GLeRoux.csv.lzma").get
+
+    MoveMatrix.allMoves.getAll(newMatrix).toSeq.map {
+      v=>
+      val loc = v._1
+        val valuesRes = Array[AnyRef](
+          geomfactory.createPoint(new Coordinate(x_laea_min + loc._1 * 1000, y_laea_min + loc._2 * 1000))
+        )
+        val simpleFeatureRes = writerRes.next
+        simpleFeatureRes.setAttributes(valuesRes)
+        writerRes.write
+    }
+
+    writerRes.close
+  }
+  val path = File("../données/EGT 2010/presence semaine EGT")
+  val outputPath = File("results")
+  outputPath.createDirectories()
+  val outFileRes = outputPath / "TEST_DEST.shp"
+  flowsFromEGT(path / "presence_semaine_GLeRoux.csv.lzma",outFileRes)
+}
